078b29fc378302bbbe796698577d8265ca163542
Eric Ingland <4283616+eingland@users.noreply.github.com>
2021-12-13 19:57:45 -0600

    Switch to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,32 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps } from 'next'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-
-import { getSortedPostsData } from '../lib/posts'
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-
-export default function Home ({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <h1>Hi, I'm Eric Ingland!</h1>
+      <h1>Hi, I'm Eric Ingland!</h1>
         <p>I'm a <b>DevOps Engineer</b>.
               I love all things computer science.
               Please have a look at my projects on <a href="http://github.com/eingland">Github</a>.</p>
-        <p>Contact me at <a href="http://twitter.com/ericingland">@ericingland</a> or by <a href="mailto:ingland.eric@gmail.com">email</a>.</p>
+        <p>Contact me at on <a href="http://linkedin.com/in/ericingland">LinkedIn</a> or by <a href="mailto:ingland.eric@gmail.com">Email</a>.</p>
         <ul style={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <li className="nav-link">
             <a href="http://github.com/eingland">
@@ -71,4 +68,13 @@ export default function Home ({ allPostsData }) {
       </section>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
