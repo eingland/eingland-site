b803c3def6600e50bc80cc51f97dd3774420bd63
Eric Ingland <4283616+eingland@users.noreply.github.com>
2021-12-17 12:42:37 -0600

    Test fix for GA4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,8 @@ import "prismjs/themes/prism-coy.css";
 import ReactGA from 'react-ga4'
 
 function App({ Component, pageProps }: AppProps) {
-  ReactGA.initialize(`${297140822}`);
+  ReactGA.initialize(`${'297140822'}`);
+  ReactGA.send("pageview");
   return <Component {...pageProps} />
 }
 
