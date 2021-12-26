8db968fdf500cf79908a543e24ed273aa6613aaa
Eric Ingland <4283616+eingland@users.noreply.github.com>
2021-12-26 13:04:24 -0600

    fix colormodescript location

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import "prismjs/themes/prism-coy.css";
 import ReactGA from "react-ga4";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeScript } from '@chakra-ui/react'
+import theme from "../components/theme";
 
 function App({ Component, pageProps }: AppProps) {
   ReactGA.initialize(`${"G-WPWCLRJ6B0"}`);
   ReactGA.send("pageview");
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>      
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Component {...pageProps} />
     </ChakraProvider>
   );
