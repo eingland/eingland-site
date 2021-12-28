aae34a42b12995bceceb5e5ee157722039017505
Eric Ingland <4283616+eingland@users.noreply.github.com>
2021-12-27 20:58:55 -0600

    Update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,21 +1,104 @@
-import { Button, Link, useColorMode } from "@chakra-ui/react";
+import { ReactNode } from 'react';
+import { Box,
+    Flex,
+    HStack,
+    Link,
+    IconButton,
+    Button,
+    Menu,
+    useDisclosure,
+    useColorModeValue,
+    Stack,
+    useColorMode } from "@chakra-ui/react";
 import NextLink from 'next/link'
 
-const Navbar = () => {
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon, DownloadIcon } from '@chakra-ui/icons';
+
+const Links = ['blog'];
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+    <NextLink href={'/' + children} passHref>
+        <Link
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+            textDecoration: 'none',
+            bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        textTransform={'capitalize'}
+        href={'#'}>
+        {children}
+        </Link>
+    </NextLink>
+  );
+
+  export default function Navbar() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     const { colorMode, toggleColorMode } = useColorMode()
     return (
-        <div >
+      <>
+        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} mb={2}>
+          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+            <IconButton
+              size={'md'}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={'Open Menu'}
+              display={{ md: 'none' }}
+              onClick={isOpen ? onClose : onOpen}
+            />
+            <HStack spacing={8} alignItems={'center'}>
             <NextLink href={'/'} passHref>
-                <Link m={2}>Home</Link>
+                <Link
+                px={2}
+                py={1}
+                rounded={'md'}
+                _hover={{
+                    textDecoration: 'none',
+                    bg: useColorModeValue('gray.200', 'gray.700'),
+                }}
+                href={'#'}>
+                Eric Ingland
+                </Link>
             </NextLink>
-            <NextLink href={'/blog'} passHref>
-                <Link m={2}>Blog</Link>
-            </NextLink>
-            <Button onClick={toggleColorMode} m={2}>
-            Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-            </Button>
-        </div>
+              <HStack
+                as={'nav'}
+                spacing={4}
+                display={{ base: 'none', md: 'flex' }}>
+                {Links.map((link) => (
+                  <NavLink key={link}>{link}</NavLink>
+                ))}
+              </HStack>
+            </HStack>
+            <Flex alignItems={'center'}>
+              <Menu>
+                <Link href="/media/Resume_EricIngland_Web.pdf">
+                    <Button
+                    variant={'solid'}
+                    colorScheme={'blue'}
+                    size={'sm'}
+                    mr={4}
+                    leftIcon={<DownloadIcon />}>
+                    Resume
+                    </Button>
+                </Link>
+                <Button onClick={toggleColorMode} m={2}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                </Button>
+              </Menu>
+            </Flex>
+          </Flex>
+  
+          {isOpen ? (
+            <Box pb={4} display={{ md: 'none' }}>
+              <Stack as={'nav'} spacing={4}>
+                {Links.map((link) => (
+                  <NavLink key={link}>{link}</NavLink>
+                ))}
+              </Stack>
+            </Box>
+          ) : null}
+        </Box>
+        </>
     );
-};
-
-export default Navbar;
+  }
